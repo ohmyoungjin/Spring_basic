67d2b9998ba02ac2edc1a032b0fb2568389da145
ohmyoungjin <sisaudwls@naver.com>
2022-10-21 13:03:14 +0900

    20221021 :: AppConfig 리펙터링

--- src/main/java/Hello/core/AppConfig.java
+++ src/main/java/Hello/core/AppConfig.java
@@ -1,5 +1,6 @@
 package Hello.core;
 
+import Hello.core.discount.DiscountPolicy;
 import Hello.core.discount.FixDiscountPolicy;
 import Hello.core.discount.RateDiscountPolicy;
 import Hello.core.member.MemberService;
@@ -10,13 +11,30 @@ import Hello.core.order.OrderServiceImpl;
 
 //구현 객체를 생성하고, 연결하는 책임을 가지는 별도의 설정 클래스
 public class AppConfig {
-
+//    리팩터링 전
+//    public MemberService memberService() {
+//        return new MemberServiceImpl(new MemoryMemberRepository());
+//    }
+//    public OrderService orderService() {
+//        return new OrderServiceImpl(new MemoryMemberRepository(), new RateDiscountPolicy());
+//     }
+//리팩터링 후 new MemoryMemberRepository() 가 두 번 쓰이는 것을 한 번에 묶어서 해줌
+//  얻을 수 있는 이점 : AppConfig 를 보면 역할과 구현 클래스가 한눈에 들어온다. 애플리케이션 전체 구성이 어떻게 되어있는지
+//    빠르게 파악할 수 있다.
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new RateDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 
 }
