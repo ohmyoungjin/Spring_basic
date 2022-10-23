7e0f5e8bc5614b7eee4f1f50a391b1bd0bdad23c
ohmyoungjin <sisaudwls@naver.com>
2022-10-23 20:58:21 +0900

    20221023 :: 싱글톤 방식의 주의점2

--- src/test/java/Hello/core/singleton/StateFulServiceTest.java
+++ src/test/java/Hello/core/singleton/StateFulServiceTest.java
@@ -22,9 +22,13 @@ class StateFulServiceTest {
         stateFulService2.order("userB", 20000);
 
         //ThreadA : 사용자 A 주문 금액 조회
-        int price = stateFulService1.getPrice();
-        System.out.println("price : " + price);
-        Assertions.assertThat(stateFulService1.getPrice()).isEqualTo(20000);
+        //싱글톤 공유필드 잘못설정하면 문제가 생기는 부분
+        //userA에서 만원 주문을 했지만 동시에 userB가 2만원 주문을 하게 되면 공유필드에 값이 2만원이 복사 돼서
+        //userA한테도 2만원의 주문서가 가게 된다. 큰 문제다 이건 정말 !
+        //int price = stateFulService1.getPrice();
+        int userAprice = stateFulService1.order("userA", 10000);
+        System.out.println("price : " + userAprice);
+        //Assertions.assertThat(stateFulService1.getPrice()).isEqualTo(20000);
     }
 
     static class TestConfig {
