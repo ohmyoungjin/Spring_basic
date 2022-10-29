a9d81e8c29378dc44af00175ea00cb1d6be16d84
ohmyoungjin <sisaudwls@naver.com>
2022-10-29 22:23:33 +0900

    20221030 :: prototype Scope - SingletonWithPrototype2

--- src/test/java/Hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/Hello/core/scope/SingletonWithPrototypeTest1.java
@@ -2,6 +2,7 @@ package Hello.core.scope;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
@@ -22,6 +23,36 @@ public class SingletonWithPrototypeTest1 {
         Assertions.assertThat(prototypeBean2.getCount()).isEqualTo(1);
     }
 
+    @Test
+    void singletonClientUsePrototype() {
+        AnnotationConfigApplicationContext ac
+                = new AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class);
+        ClientBean clientBean1 = ac.getBean(ClientBean.class);
+        int count1 = clientBean1.logic();
+        System.out.println("count1 : " + count1);
+        Assertions.assertThat(count1).isEqualTo(1);
+
+
+        ClientBean clientBean2 = ac.getBean(ClientBean.class);
+        int count2 = clientBean2.logic();
+        System.out.println("count2 : " + count2);
+        Assertions.assertThat(count1).isEqualTo(2);
+    }
+
+    @Scope("singleton")
+    static class ClientBean {
+        private final PrototypeBean prototypeBean;
+        //생성자가 한 개 이기때문에 생략 가능 , 롬복으로 처리도 가능
+        @Autowired
+        public ClientBean(PrototypeBean prototypeBean) {
+            this.prototypeBean = prototypeBean;
+        }
+
+        public int logic() {
+            prototypeBean.addCount();
+            return prototypeBean.getCount();
+        }
+    }
     @Scope("prototype")
     static class PrototypeBean {
         private int count = 0;
