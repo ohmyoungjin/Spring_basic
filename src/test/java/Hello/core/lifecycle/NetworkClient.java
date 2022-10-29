25a3e934d766b29f42fff196ef4e9ac3dea6236b
ohmyoungjin <sisaudwls@naver.com>
2022-10-29 15:30:00 +0900

    20221029 :: bean LifeCycle call back start2

--- src/test/java/Hello/core/lifecycle/NetworkClient.java
+++ src/test/java/Hello/core/lifecycle/NetworkClient.java
@@ -5,7 +5,7 @@ public class NetworkClient {
     private String url;
 
     public NetworkClient() {
-        System.out.println("생성자 호출, url : " + url);
+        System.out.println("생성자 호출, url = " + url);
         connect();
         call("초기화 연결 메시지");
     }
@@ -19,7 +19,7 @@ public class NetworkClient {
     }
 
     public void call(String message) {
-        System.out.println("call : " + url + "message : " + message);
+        System.out.println("call : " + url + "message = " + message);
     }
 
     //서비스 종료시 호출
